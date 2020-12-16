2e1ed6ca4862db74af9a4a15836f6b7f7de90860
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-12-16 20:35:37 +0100

    handle creating

---
+++ battle_frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import warriorsService from "./services/warriors";
+import Warrior from "./components/Warrior";
+
+const App = () => {
+
+    const [newName, setNewName] = useState("")
+    const [warriors, setWarriors] = useState([])
+
+    const handleCreate = async (event) => {
+        event.preventDefault()
+
+        const warriorObj = {name: newName, id: warriors.length + 1}
+        const newWarriorPromise = warriorsService.addWarriorServer(warriorObj)
+        setNewName("")
+        const response = await newWarriorPromise
+        const newWarriors = warriors.concat(response)
+        setWarriors(newWarriors)
+
+    }
+
+    return (
+        <div>
+            <h2>Battle</h2>
+            <h4>List of warriors:</h4>
+            {warriors.map(warrior=><Warrior name={newName}/>)}
+        </div>
+    )
+}
+
+export default App;
