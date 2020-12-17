6bd43f2f660161aa2380c4e3cac3c1ddbb32c564
misladkova <michaela.sladkova@gmail.com>
2020-12-18 00:42:59 +0100

    reformat

--- battle_frontend/src/components/Warrior.js
+++ battle_frontend/src/components/Warrior.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Warrior = ({warrior, handleUpdate, handleDelete}) => {
     console.log("n", warrior)
-    return(
-        <li>bojovnik: {warrior.id} {warrior.name}<button onClick={handleUpdate}>update</button><button onClick={() =>
-            handleDelete(warrior.id)}>delete</button></li>
+    return (
+        <li>bojovnik: {warrior.id} {warrior.name}
+            <button onClick={handleUpdate}>update</button>
+            <button onClick={() =>
+                handleDelete(warrior.id)}>delete
+            </button>
+        </li>
     )
 }
 
