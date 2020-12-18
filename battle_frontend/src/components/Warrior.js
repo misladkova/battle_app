6a1a8dd5d9d029db270909caa31c7fd15bc543e9
misladkova <michaela.sladkova@gmail.com>
2020-12-18 02:19:51 +0100

    extend

--- battle_frontend/src/components/Warrior.js
+++ battle_frontend/src/components/Warrior.js
@@ -3,7 +3,8 @@ import React from "react";
 const Warrior = ({warrior, handleUpdate, handleDelete}) => {
     console.log("n", warrior)
     return (
-        <li>bojovnik: {warrior.id} {warrior.name}
+        <li>bojovnik: {warrior.id} {warrior.name}, strength: {warrior.strength}, speed: {warrior.speed},
+            toughness: {warrior.toughness}
             <button onClick={handleUpdate}>update</button>
             <button onClick={() =>
                 handleDelete(warrior.id)}>delete
