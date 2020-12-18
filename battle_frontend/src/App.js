dc7699e1ba89c32daf41288cada37f2f7d534f7e
misladkova <michaela.sladkova@gmail.com>
2020-12-18 19:21:00 +0100

    add options

--- battle_frontend/src/App.js
+++ battle_frontend/src/App.js
@@ -1,16 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Component} from "react";
 import warriorsService from "./services/warriors";
 import Warrior from "./components/Warrior";
 import WarriorForm from "./components/WarriorForm";
+import Select from "react-select";
 
 const App = () => {
 
     const [newName, setNewName] = useState("")
     const [warriors, setWarriors] = useState([])
+    const [inBattle, setInBattle] = useState([])
+    const [options, setOptions] = useState([])
 
-    async function fetchData(){
+    async function fetchData() {
         const wars = await warriorsService.getWarriorsServer()
         setWarriors(wars.data)
+        const opt = wars.data.map(x => ({
+            "value": x.id,
+            "label": x.name
+        }))
+        setOptions(opt)
     }
 
     useEffect(() => {
@@ -29,10 +37,15 @@ const App = () => {
         const newWarriors = warriors.concat(response.data)
         console.log("l", newWarriors)
         setWarriors(newWarriors)
+        const opt = newWarriors.map(x => ({
+            "value": x.id,
+            "label": x.name
+        }))
+        setOptions(opt)
+
     }
 
     const handleUpdate = async () => {
-
     }
 
     const handleDelete = async (id) => {
@@ -41,14 +54,28 @@ const App = () => {
         console.log(response)
         const newWarriors = warriors.filter(x => x.id !== id)
         setWarriors(newWarriors)
+        const opt = newWarriors.map(x => ({
+            "value": x.id,
+            "label": x.name
+        }))
+        setOptions(opt)
     }
     console.log("warriorsssss:", warriors)
+
+    const handleFight = () => {
+
+    }
+
     return (
         <div>
             <h2>Battle</h2>
             <h4>List of warriors:</h4>
-            {warriors.map(warrior => <Warrior key={warrior.id} warrior={warrior} handleDelete={handleDelete}/>)}
+            {warriors.map(warrior => <Warrior key={warrior.id} warrior={warrior} handleDelete={handleDelete}
+                                              handleUpdate={handleUpdate}/>)}
             <WarriorForm name={newName} setNewName={setNewName} handleCreate={handleCreate}/>
+            <Select options={options}/>
+            {inBattle.map(x => <Warrior key={x.id} warrior={x}/>)}
+            <button onClick={handleFight}>fight</button>
         </div>
     )
 }
