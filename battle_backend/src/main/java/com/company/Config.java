021ad3bc52d6997f1d6ff5fd5113cd9a12bcc62f
misladkova <michaela.sladkova@gmail.com>
2020-12-20 19:25:42 +0100

    update

--- battle_backend/src/main/java/com/company/Config.java
+++ battle_backend/src/main/java/com/company/Config.java
@@ -9,13 +9,16 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class Config {
 
+    String dbPass =  System.getenv("DB_PASS");
+    String dbUser = System.getenv("DB_USER");
+    String db = System.getenv("DB");
+
     public MongoClient mongoClient() {
-        String dbPass =  System.getenv("DB_PASS");
-        return MongoClients.create("mongodb+srv://mislad:"+dbPass+"@cluster0.b2qrq.mongodb.net/battle?retryWrites=true&w=majority");
+        return MongoClients.create("mongodb+srv://"+dbUser+":"+dbPass+"@cluster0.b2qrq.mongodb.net/"+db+"?retryWrites=true&w=majority");
     }
 
     public @Bean
     MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "battle");
+        return new MongoTemplate(mongoClient(), db);
     }
 }
