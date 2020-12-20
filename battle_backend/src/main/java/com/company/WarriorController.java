4f8d0d4b125cccd24455c79029e07b7903f70be4
misladkova <michaela.sladkova@gmail.com>
2020-12-20 13:10:56 +0100

    modify methods

--- battle_backend/src/main/java/com/company/WarriorController.java
+++ battle_backend/src/main/java/com/company/WarriorController.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import com.mongodb.client.result.DeleteResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +14,8 @@ import java.util.*;
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class WarriorController {
+    @Autowired
+    MongoTemplate mongoTemplate;
     private static Map<String, Warrior> warriorRegister = new HashMap<>();
     static {
         Warrior first = new Warrior();
@@ -43,7 +50,11 @@ public class WarriorController {
 
     @RequestMapping(value = "/warriors/{id}")
     public ResponseEntity<Warrior> getWarrior(@PathVariable("id") String id){
-        return new ResponseEntity<>(warriorRegister.get(id), HttpStatus.OK);
+        Criteria criteria = Criteria.where("id").is(id);
+        Query query = Query.query(criteria);
+        Warrior warrior1 = mongoTemplate.findOne(query, Warrior.class);
+//        System.out.println(warrior1);
+        return new ResponseEntity<>(warrior1, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/warriors", method = RequestMethod.POST)
@@ -62,7 +73,11 @@ public class WarriorController {
 
     @RequestMapping(value = "/warriors/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<String> delete(@PathVariable("id") String id) {
-        warriorRegister.remove(id);
+//        warriorRegister.remove(id);
+        Criteria criteria1 = Criteria.where("id").is(id);
+        Query query1 = Query.query(criteria1);
+        DeleteResult deleteResult= mongoTemplate.remove(query1, Warrior.class);
+//        System.out.println("Deleted documents: " + deleteResult.getDeletedCount());
         return new ResponseEntity<>("Warrior was deleted", HttpStatus.OK);
     }
 
