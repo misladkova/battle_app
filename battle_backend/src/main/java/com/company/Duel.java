a568a518c2181a26763e134e368ca8a676d50b70
misladkova <michaela.sladkova@gmail.com>
2020-12-22 10:55:15 +0100

    cleaning

--- battle_backend/src/main/java/com/company/Duel.java
+++ battle_backend/src/main/java/com/company/Duel.java
@@ -5,32 +5,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "duels")
 public class Duel {
 
-//    private String rivalName1;
-//    private String rivalName2;
     private Warrior rival1;
     private Warrior rival2;
     private String winner;
-
-//    public Duel(String rivalName1, String rivalName2){
-//        this.rivalName1 = rivalName1;
-//        this.rivalName2 = rivalName2;
-//        calculate();
-//    }
 
     public Duel(Warrior rival1, Warrior rival2){
         this.rival1 = rival1;
         this.rival2 = rival2;
         calculate();
     }
-
-//    public String getRivalName1(){
-//        return rivalName1;
-//    }
-//
-//    public String getRivalName2(){
-//        return rivalName2;
-//    }
-
 
     public Warrior getRival1() {
         return rival1;
@@ -39,11 +22,6 @@ public class Duel {
     public Warrior getRival2() {
         return rival2;
     }
-
-//    public String getWinner(){
-//        return winner;
-//    }
-
 
     public String getWinner() {
         return winner;
