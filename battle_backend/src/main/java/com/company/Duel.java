4838f376473d707dffb3f5fe54715a001e276cb1
misladkova <michaela.sladkova@gmail.com>
2020-12-22 10:54:28 +0100

    modify method

--- battle_backend/src/main/java/com/company/Duel.java
+++ battle_backend/src/main/java/com/company/Duel.java
@@ -5,29 +5,57 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "duels")
 public class Duel {
 
-    private String rivalName1;
-    private String rivalName2;
+//    private String rivalName1;
+//    private String rivalName2;
+    private Warrior rival1;
+    private Warrior rival2;
     private String winner;
 
-    public Duel(String rivalName1, String rivalName2){
-        this.rivalName1 = rivalName1;
-        this.rivalName2 = rivalName2;
+//    public Duel(String rivalName1, String rivalName2){
+//        this.rivalName1 = rivalName1;
+//        this.rivalName2 = rivalName2;
+//        calculate();
+//    }
+
+    public Duel(Warrior rival1, Warrior rival2){
+        this.rival1 = rival1;
+        this.rival2 = rival2;
         calculate();
     }
 
-    public String getRivalName1(){
-        return rivalName1;
+//    public String getRivalName1(){
+//        return rivalName1;
+//    }
+//
+//    public String getRivalName2(){
+//        return rivalName2;
+//    }
+
+
+    public Warrior getRival1() {
+        return rival1;
     }
 
-    public String getRivalName2(){
-        return rivalName2;
+    public Warrior getRival2() {
+        return rival2;
     }
 
-    public String getWinner(){
+//    public String getWinner(){
+//        return winner;
+//    }
+
+
+    public String getWinner() {
         return winner;
     }
 
     private void calculate(){
-        winner = rivalName1;
+        double result1 = rival1.getStrength()*Math.random()+ rival1.getSpeed()*Math.random()+ rival1.getToughness()*Math.random();
+        double result2 = rival2.getStrength()*Math.random()+ rival2.getSpeed()*Math.random()+ rival2.getToughness()*Math.random();
+        if(result1>result2) {
+            winner = rival1.getName();
+        }else{
+            winner = rival2.getName();
+        }
     }
 }
