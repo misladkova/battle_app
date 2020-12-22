caf3f0b675e1c39b19d2cbaf0cfaed2e80cc515e
misladkova <michaela.sladkova@gmail.com>
2020-12-22 18:44:28 +0100

    add file

--- battle_frontend/src/components/CreateForm.js
+++ battle_frontend/src/components/CreateForm.js
@@ -4,16 +4,35 @@ import React, {useState} from "react";
 const CreateForm = ({warriors, setWarriors, setCreateVisible}) => {
 
     const [newName, setNewName] = useState("")
+    const [newFile, setNewFile] = useState("")
 
     const handleCreate = async (event) => {
         event.preventDefault()
 
-        const warriorObj = {name: newName}
+        const warriorObj = {name: newName, file: newFile}
         const newWarriorPromise = warriorsService.addWarriorServer(warriorObj)
         setNewName("")
+        setNewFile("")
         const response = await newWarriorPromise
         const newWarriors = warriors.concat(response.data)
         setWarriors(newWarriors)
+    }
+
+    const fileOnChange = (e) => {
+        let file = e.target.files[0]
+
+        if(file){
+            const reader = new FileReader()
+            reader.onload = handleReaderLoaded
+            reader.readAsBinaryString(file)
+        }
+    }
+
+    const handleReaderLoaded = (readerEvent) => {
+        let binaryString = readerEvent.target.result
+        const x = btoa(binaryString)
+        console.log("loaded", x)
+        setNewFile(x)
     }
 
     return(
@@ -23,6 +42,7 @@ const CreateForm = ({warriors, setWarriors, setCreateVisible}) => {
                 <div>
                     Name: <input id="title" type="text" value={newName} onChange={({target}) =>
                     setNewName(target.value)}/>
+                    Picture: <input id="image" type="file" accept=".jpeg, .png, .jpg" onChange={fileOnChange}/>
                 </div>
                 <button id="create-button" type="submit">add</button> <button onClick={() => setCreateVisible(false)}>cancel</button>
             </form>
