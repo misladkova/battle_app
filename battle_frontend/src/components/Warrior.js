d8a506e11609f71529b881e06c9010953b2c3acf
misladkova <michaela.sladkova@gmail.com>
2020-12-22 18:43:49 +0100

    add image

--- battle_frontend/src/components/Warrior.js
+++ battle_frontend/src/components/Warrior.js
@@ -18,6 +18,8 @@ const Warrior = ({warrior, warriors, setWarriors, setUpId, setUpdateVisible}) =>
     return (
         <li>Warrior: {warrior.id} {warrior.name}, strength: {warrior.strength}, speed: {warrior.speed},
             toughness: {warrior.toughness}
+
+            <img src={`data:image/jpeg;base64,${warrior.file}`} alt="aaa" width="50"/>
             <button onClick={() => handleUpdate(warrior.id)}>update</button>
             <button onClick={() => handleDelete(warrior.id)}>delete</button>
         </li>
