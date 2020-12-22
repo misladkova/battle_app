38b1ec29503329c41af56395b15153a16742a041
misladkova <michaela.sladkova@gmail.com>
2020-12-22 19:19:53 +0100

    add file

--- battle_frontend/src/components/UpdateForm.js
+++ battle_frontend/src/components/UpdateForm.js
@@ -4,18 +4,38 @@ import warriorsService from "../services/warriors";
 const UpdateForm = ({warriors, setWarriors, upId, setUpdateVisible}) => {
 
     const [updatedName, setUpdatedName] = useState("")
+    const [updatedFile, setUpdatedFile] = useState("")
 
     const handleChange = async (event) => {
         event.preventDefault()
 
         const x = warriors.find(a=>a.id===upId)
-        const updatedWarrior = {...x, name: updatedName}
+        const updatedWarrior = {...x, name: updatedName, file: updatedFile}
         const newWarriorPromise = warriorsService.updateWarriorServer(upId, updatedWarrior)
         const response = await newWarriorPromise
-        const newWarriors = warriors.map(w=> w.name!==x.name ? w: response.data)
+        const newWarriors = warriors.map(w=> (w.name!==x.name||w.file!==x.file) ? w: response.data)
         setWarriors(newWarriors)
         setUpdatedName("")
+        setUpdatedFile("")
     }
+
+    const fileChange = (e) => {
+        let file = e.target.files[0]
+
+        if(file){
+            const reader = new FileReader()
+            reader.onload = handleReaderLoaded
+            reader.readAsBinaryString(file)
+        }
+    }
+
+    const handleReaderLoaded = (readerEvent) => {
+        let binaryString = readerEvent.target.result
+        const x = btoa(binaryString)
+        console.log("loaded", x)
+        setUpdatedFile(x)
+    }
+
     return(
         <div>
             <h4>Update the warrior:</h4>
@@ -23,6 +43,7 @@ const UpdateForm = ({warriors, setWarriors, upId, setUpdateVisible}) => {
                 <div>
                     New name: <input type="text" value={updatedName} onChange={({target}) =>
                     setUpdatedName(target.value)}/>
+                    New file: <input type="file" accept=".jpeg, .png, .jpg" onChange={fileChange}/>
                 </div>
                 <button id="update-button">change</button> <button onClick={() => setUpdateVisible(false)}>cancel</button>
             </form>
