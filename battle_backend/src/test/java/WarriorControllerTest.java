a4caecd5e501cc8ba12309f9f55767f9540ccb6e
misladkova <michaela.sladkova@gmail.com>
2020-12-28 00:02:35 +0100

    cleaning

--- battle_backend/src/test/java/WarriorControllerTest.java
+++ battle_backend/src/test/java/WarriorControllerTest.java
@@ -1,16 +1,10 @@
-import com.company.Config;
+
 import com.company.Warrior;
-import com.company.WarriorController;
-import com.company.WarriorRepository;
+
+
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
