07dd758420f4b9e967c2a598843f0eff52abe266
misladkova <michaela.sladkova@gmail.com>
2021-01-02 18:45:14 +0100

    update

--- battle_frontend/cypress/integration/battle_app.spec.js
+++ battle_frontend/cypress/integration/battle_app.spec.js
@@ -12,22 +12,10 @@ describe('Battle app', function () {
     describe('buttons', function () {
         it('adding', function () {
             cy.contains('add new warrior').click()
-            cy.get('#nameCreate').type('bm')
+            cy.contains('Create new warrior:')
+            cy.get('#title')
+            cy.get('#image')
             cy.get('#create-button').click()
-            // cy.contains('Ben Murph is logged in')
         })
     })
-
-    // describe('When user is logged in', function (){
-    //     beforeEach(function (){
-    //         cy.contains('Log in').click()
-    //         cy.get('#username').type('bmurph')
-    //         cy.get('#password').type('bmurph')
-    //         cy.get('#login-button').click()
-    //         cy.contains('Ben Murph is logged in')
-    //     })
-    //     it('A new blog can be created', function (){
-    //
-    //     })
-    // })
 })
