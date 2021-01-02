b631c8833e55a577cbe379eb6340c59f9f020bc2
misladkova <michaela.sladkova@gmail.com>
2021-01-02 17:49:56 +0100

    define component

---
+++ battle_frontend/src/components/Warriors.js
@@ -0,0 +1,24 @@
+import Warrior from "./Warrior";
+import React from "react";
+
+const Warriors = ({warriors, setWarriors, setUpdateVisible, setUpId}) => {
+    console.log("aaaaa", warriors)
+    // https://stackoverflow.com/questions/62880615/how-do-i-map-for-every-two-elements-for-react
+    const rows = warriors.reduce(function (rows, key, index) {
+        return (index % 3 === 0 ? rows.push([key])
+            : rows[rows.length-1].push(key)) && rows;
+    }, []);
+    return (
+        <div className="mt-4">
+            {rows.map(row => (
+                <div className="row mt-3 mb-3">
+                    {row.map(warrior => <Warrior key={warrior.id} warrior={warrior} warriors={warriors}
+                                                     setWarriors={setWarriors} setUpdateVisible={setUpdateVisible}
+                                                     setUpId={setUpId}/>)}
+                </div>
+            )) }
+        </div>
+    )
+}
+
+export default Warriors
