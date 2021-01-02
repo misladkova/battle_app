33406241e70476f5627cf994c7c566972aa28e6c
misladkova <michaela.sladkova@gmail.com>
2021-01-02 17:57:13 +0100

    modify

--- battle_frontend/src/components/Warrior.js
+++ battle_frontend/src/components/Warrior.js
@@ -16,14 +16,47 @@ const Warrior = ({warrior, warriors, setWarriors, setUpId, setUpdateVisible}) =>
     }
 
     return (
-        <li>Warrior: {warrior.id} {warrior.name}, strength: {warrior.strength}, speed: {warrior.speed},
-            toughness: {warrior.toughness}
+        // <li>Warrior: {warrior.id} {warrior.name}, strength: {warrior.strength}, speed: {warrior.speed},
+        //     toughness: {warrior.toughness}
+        //
+        //     <img src={`data:image/jpeg;base64,${warrior.file}`} alt="aaa" width="50"/>
+        //     <button onClick={() => handleUpdate(warrior.id)}>update</button>
+        //     <button onClick={() => handleDelete(warrior.id)}>delete</button>
+        // </li>
 
-            <img src={`data:image/jpeg;base64,${warrior.file}`} alt="aaa" width="50"/>
-            <button onClick={() => handleUpdate(warrior.id)}>update</button>
-            <button onClick={() => handleDelete(warrior.id)}>delete</button>
-        </li>
-    )
-}
+        // <div className="card">
+        //      <img className="card-img-top" src={`data:image/jpeg;base64,${warrior.file}`} alt="aaa" width="100" height="100"/>
+        //          <div className="card-body">
+        //              <h5 className="card-title">Warrior: {warrior.name}</h5>
+        //              <p className="card-text">strength: {warrior.strength}, speed: {warrior.speed},
+        //                              toughness: {warrior.toughness}</p>
+        //              <p className="card-text"><small className="text-muted">{warrior.id}</small></p>
+        //          </div>
+        // </div>
+        // <div className="row mt-3">
+        // <div className="card-deck">
+        //     <div className="card">
+        //         <img className="card-img-top" src="..." alt="Card image cap"/>
+        //             <div className="card-body">
+        //                 <h5 className="card-title">Card title</h5>
+        //                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in
+        //                     to additional content. This content is a little bit longer.</p>
+        //                 <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        //             </div>
+        //     </div>
+        <div className="col-md-4">
+            <div className="card">
+                <img className="card-img-top" src="..." alt="Card image cap"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This card has supporting text below as a natural lead-in to additional
+                            content.</p>
+                        <button onClick={() => handleUpdate(warrior.id)}>update</button>
+                        <button onClick={() => handleDelete(warrior.id)}>delete</button>
+                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+            </div>
+        </div>
+)}
 
 export default Warrior
