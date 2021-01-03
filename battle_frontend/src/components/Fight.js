fd240523e6bdd72dc04fddbfd00226ff02d5c77a
misladkova <michaela.sladkova@gmail.com>
2021-01-03 02:46:49 +0100

    add key

--- battle_frontend/src/components/Fight.js
+++ battle_frontend/src/components/Fight.js
@@ -17,6 +17,8 @@ const Fight = ({options, setDuels, setFightVisible}) => {
     }
 
     const handleFight = async (id1, id2) => {
+        id1 = id1 ? id1 : options[0].value;
+        id2 = id2 ? id2 : options[1].value;
         const newWarriorPromise = warriorsService.getBattleServer(id1, id2)
         const response = await newWarriorPromise
         setDuels(response.data)
@@ -26,11 +28,11 @@ const Fight = ({options, setDuels, setFightVisible}) => {
         <div>
             <h4>Choose first player:</h4>
             <select className="form-control" value={firstSelect} onChange={handleFirstSelect}>
-                {options.map(o => <option value={o.value}>{o.label}</option>)}
+                {options.map((o, idx) => <option key={idx} value={o.value}>{o.label}</option>)}
             </select>
             <h4>Choose second player:</h4>
             <select className="form-control" value={secondSelect} onChange={handleSecondSelect}>
-                {options.map(o => <option value={o.value}>{o.label}</option>)}
+                {options.map((o, idx) => <option key={idx} value={o.value}>{o.label}</option>)}
             </select>
             <button id="fight" className="btn btn-primary mr-2 mt-2 mb-2" onClick={() =>
                 handleFight(firstSelect, secondSelect)}>fight</button>
