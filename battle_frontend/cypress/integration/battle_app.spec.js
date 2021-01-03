05e050798e2ccd37fa3703eaea8a7b9c46a2c55e
misladkova <michaela.sladkova@gmail.com>
2021-01-03 01:51:57 +0100

    add tests

--- battle_frontend/cypress/integration/battle_app.spec.js
+++ battle_frontend/cypress/integration/battle_app.spec.js
@@ -9,13 +9,37 @@ describe('Battle app', function () {
         cy.contains('History of battles:')
     })
 
-    describe('buttons', function () {
-        it('adding', function () {
+    describe('forms', function () {
+        it('createForm is shown', function () {
             cy.contains('add new warrior').click()
             cy.contains('Create new warrior:')
             cy.get('#title')
             cy.get('#image')
             cy.get('#create-button').click()
+        })
+
+        it('createForm is hidden', function () {
+            cy.contains('add new warrior').click()
+            cy.contains('cancel').click()
+            cy.contains('Create new warrior:').should('not.exist')
+            cy.get('#title').should('not.not.exist')
+            cy.get('#image').should('not.not.exist')
+            cy.get('#create-button').should('not.not.exist')
+        })
+
+        it('fight is shown', function () {
+            cy.contains('play a battle').click()
+            cy.contains('Choose first player:')
+            cy.contains('Choose second player:')
+            cy.get('#fight').click()
+        })
+
+        it('fight is hidden', function () {
+            cy.contains('play a battle').click()
+            cy.contains('cancel').click()
+            cy.contains('Choose first player:').should('not.not.exist')
+            cy.contains('Choose second player:').should('not.not.exist')
+            cy.get('#fight').should('not.not.exist')
         })
     })
 })
