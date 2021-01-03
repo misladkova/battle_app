747c8fa921a4e43c07f7ba6e1cdfb37e098301c9
misladkova <michaela.sladkova@gmail.com>
2021-01-03 03:05:09 +0100

    modify

--- battle_frontend/src/components/Duel.js
+++ battle_frontend/src/components/Duel.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Duel = ({duel}) => {
+    let winner_str = "draw"
+    if(duel.winner === 1){
+        winner_str = duel.rival2.name
+    } else if (duel.winner===-1){
+        winner_str = duel.rival1.name
+    }
     return(
-        <li>{duel.rival1.name} vs {duel.rival2.name}, winner: {duel.winner}</li>
+        <li>{duel.rival1.name} vs {duel.rival2.name}, winner: {winner_str}</li>
     )
 }
 
